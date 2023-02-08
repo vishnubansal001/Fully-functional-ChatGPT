8af2fc902e6af5085c6d8af3e2fda158069155ed
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-02-08 19:02:19 +0530

    created a simple tmplate and started the frontend part of chatgpt

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="side-menu">
+        <h1>Side Menu</h1>
+      </aside>
+      <section className="chatbox">
+        <hi> section</hi>
+      </section>
     </div>
   );
 }
